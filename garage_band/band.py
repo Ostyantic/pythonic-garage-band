86d97090f842e6b39dcbe3fcff45b18fe4565fec
Anthony Lopez <Antl21292@gmail.com>
2023-03-29 22:37:27 -0400

    created Musician class and Guitarist subclass

--- garage_band/band.py
+++ garage_band/band.py
@@ -8,3 +8,22 @@ class Band:
     @staticmethod
     def play_solo(solo):
         print(solo)
+
+
+class Musician:
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Guitarist(Musician):
+
+    def __str__(self):
+        print(f'Wassup! My name is {self.name}')
+
+    def __repr__(self):
+        print(f'Guitarist("{self.name}")')
+
+    def get_instrument(self):
+        print('Instrument: Guitar')
+
