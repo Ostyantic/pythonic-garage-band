2a814a34ae796468c75bb96abeb7ad015577a5b6
Anthony Lopez <Antl21292@gmail.com>
2023-03-29 22:52:19 -0400

    Bassist and Drummer sub-classes created

--- garage_band/band.py
+++ garage_band/band.py
@@ -19,11 +19,61 @@ class Musician:
 class Guitarist(Musician):
 
     def __str__(self):
-        print(f'Wassup! My name is {self.name}')
+        return f'My name is {self.name} and I play {self.get_instrument()}'
 
     def __repr__(self):
-        print(f'Guitarist("{self.name}")')
+        return f'Guitarist("{self.name}")'
 
+    @staticmethod
     def get_instrument(self):
-        print('Instrument: Guitar')
+        return 'guitar'
 
+    @staticmethod
+    def play_solo(self):
+        return 'face melting guitar solo'
+
+
+class Bassist(Musician):
+
+    def __str__(self):
+        return f'My name is {self.name} and I play {self.get_instrument()}'
+
+    def __repr__(self):
+        return f'Bassist("{self.name}")'
+
+    @staticmethod
+    def get_instrument(self):
+        return 'bass'
+
+    @staticmethod
+    def play_solo(self):
+        return 'bom bom buh bom'
+
+
+class Drummer(Musician):
+
+    def __str__(self):
+        return f'My name is {self.name} and I play {self.get_instrument()}'
+
+    def __repr__(self):
+        return f'Bassist("{self.name}")'
+
+    @staticmethod
+    def get_instrument(self):
+        return 'bass'
+
+    @staticmethod
+    def play_solo(self):
+        return 'bom bom buh bom'
+
+
+if __name__ == "__main__":
+    # Guitarists
+    joan = pass
+    jimi = pass
+    # Drummers
+    sheila = pass
+    ginger = pass
+    # Bassists
+    meshell = pass
+    flea = pass
